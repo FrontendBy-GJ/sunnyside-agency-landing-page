0c5bfea55f63102403daf250e84ee2c1e3d89b6c
Genaro Garcia <genaro.dev@outlook.com>
2023-05-17 15:03:25 -0500

    Add Testimonial component

---
+++ src/components/Testimonial.jsx
@@ -0,0 +1,18 @@
+const Testimonial = ({ img, quote, name, jobTitle }) => {
+  return (
+    <div className="text-center">
+      <img
+        src={img}
+        alt={name}
+        className="mx-auto aspect-square w-16 rounded-full"
+      />
+      <p className="mt-5 font-Barlow text-slate-600">{quote}</p>
+      <span className="mt-6 block font-Fraunces text-lg font-black">
+        {name}
+      </span>
+      <span className="mt-2 block font-Barlow text-slate-400">{jobTitle}</span>
+    </div>
+  );
+};
+
+export default Testimonial;
