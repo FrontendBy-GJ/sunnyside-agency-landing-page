21c01a3f5abbffb15dac3db24317e2937042734c
Genaro Garcia <genaro.dev@outlook.com>
2023-05-20 06:15:35 -0500

    Add image imports and exports

---
+++ src/assets/images.js
@@ -0,0 +1,47 @@
+import header from '../assets/mobile/image-header.jpg';
+import headerDesktop from '../assets/desktop/image-header.jpg';
+import egg from '../assets/mobile/image-transform.jpg';
+import eggDesktop from '../assets/desktop/image-transform.jpg';
+import cup from '../assets/mobile/image-stand-out.jpg';
+import cupDesktop from '../assets/desktop/image-stand-out.jpg';
+import cherries from '../assets/mobile/image-graphic-design.jpg';
+import cherriesDesktop from '../assets/desktop/image-graphic-design.jpg';
+import orange from '../assets/mobile/image-photography.jpg';
+import orangeDesktop from '../assets/desktop/image-photography.jpg';
+import emily from '../assets/image-emily.jpg';
+import thomas from '../assets/image-thomas.jpg';
+import jennie from '../assets/image-jennie.jpg';
+import milkBottles from '../assets/mobile/image-gallery-milkbottles.jpg';
+import milkBottlesDesktop from '../assets/desktop/image-gallery-milkbottles.jpg';
+import orangeHalf from '../assets/mobile/image-gallery-orange.jpg';
+import orangeHalfDesktop from '../assets/desktop/image-gallery-orange.jpg';
+import cone from '../assets/mobile/image-gallery-cone.jpg';
+import coneDesktop from '../assets/desktop/image-gallery-cone.jpg';
+import sugarCubes from '../assets/mobile/image-gallery-sugar-cubes.jpg';
+import sugarCubesDesktop from '../assets/desktop/image-gallery-sugarcubes.jpg';
+import Logo from '../assets/Logo';
+
+export {
+  header,
+  headerDesktop,
+  egg,
+  eggDesktop,
+  cup,
+  cupDesktop,
+  cherries,
+  cherriesDesktop,
+  orange,
+  orangeDesktop,
+  emily,
+  thomas,
+  jennie,
+  milkBottles,
+  milkBottlesDesktop,
+  orangeHalf,
+  orangeHalfDesktop,
+  cone,
+  coneDesktop,
+  sugarCubes,
+  sugarCubesDesktop,
+  Logo,
+};
